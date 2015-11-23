df862be9b14551d4b1cc2d43433fca517df3d2ec
Brett Lilley <brettlilley@bretts-mbp-2.cape-link.com>
2015-11-23 08:39:50 -0500

    First passing test

--- ts/src/main/app/primefactors.ts
+++ ts/src/main/app/primefactors.ts
@@ -1,5 +1,5 @@
 export default class PrimeFactors{
     public static generate(value:number): any{
-        return null;
+        return [];
     }
 };
--- ts/src/test/app/primefactors-spec.ts
+++ ts/src/test/app/primefactors-spec.ts
@@ -3,7 +3,7 @@ import PrimeFactors from '../../main/app/primefactors';
 describe('Prime Factors', () => {
     describe('primeFactors of 1', () => {
         it('should return empty array', () => {
-            expect(PrimeFactors.generate(1)).toBe([]);
+            expect(PrimeFactors.generate(1)).toEqual([]);
         });
     });
 });
