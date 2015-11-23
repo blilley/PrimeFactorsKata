d13095a6fb4e902c1fe62c695a77338050344696
Brett Lilley <brettlilley@bretts-mbp-2.cape-link.com>
2015-11-23 09:18:38 -0500

    Second passing test

--- ts/src/main/app/primefactors.ts
+++ ts/src/main/app/primefactors.ts
@@ -1,5 +1,9 @@
 export default class PrimeFactors{
-    public static generate(value:number): any{
-        return [];
+    public static generate(value:number): any {
+        var primes = [];
+        if(value > 1){
+            primes.push(2);
+        }
+        return primes;
     }
 };
