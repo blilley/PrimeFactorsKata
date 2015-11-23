986c2ef939a21856b5226372dd2ca4357a98f7b9
Brett Lilley <brettlilley@bretts-mbp-2.cape-link.com>
2015-11-23 11:41:16 -0500

    Three passing tests

--- java/src/test/java/com/pillar/PrimeFactorsTest.java
+++ java/src/test/java/com/pillar/PrimeFactorsTest.java
@@ -5,22 +5,27 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PrimeFactorsTest {
     @Test
-    public void testOne(){
-        assertThat(PrimeFactors.generate(1), is(empty()));
+    public void generate_When1_ReturnsEmptyList(){
+        assertThat(PrimeFactors.generate(1), is(list()));
     }
 
     @Test
-    public void testTwo(){
-        assertThat(PrimeFactors.generate(2), is(list()));
+    public void generate_When2_ReturnsListWith2(){
+        assertThat(PrimeFactors.generate(2), is(list(2)));
     }
-    
-    private List<Integer> list() {
-        return Lists.newArrayList(2);
+
+    @Test
+    public void generate_When3_ReturnsListWith3(){
+        assertThat(PrimeFactors.generate(3), is(list(3)));
+    }
+
+
+    private List<Integer> list(Integer... values) {
+        return Lists.newArrayList(values);
     }
 }
