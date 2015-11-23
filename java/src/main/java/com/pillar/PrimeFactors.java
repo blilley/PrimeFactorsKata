c187f99e2f5f044f5d08f1489d0610c35ba11571
Brett Lilley <brettlilley@bretts-mbp-2.cape-link.com>
2015-11-23 11:12:42 -0500

    First passing test

--- java/src/main/java/com/pillar/PrimeFactors.java
+++ java/src/main/java/com/pillar/PrimeFactors.java
@@ -1,4 +1,11 @@
 package com.pillar;
 
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
 public class PrimeFactors {
+    public static List<Integer> generate(int value) {
+        return Lists.newArrayList();
+    }
 }
--- java/src/test/java/com/pillar/PrimeFactorsTest.java
+++ java/src/test/java/com/pillar/PrimeFactorsTest.java
@@ -1,11 +1,18 @@
 package com.pillar;
 
+import com.google.common.collect.Lists;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
 
 public class PrimeFactorsTest {
     @Test
-    public void nothing(){
-
+    public void testOne(){
+        List<Integer> expected = Lists.newArrayList();
+        assertThat(PrimeFactors.generate(1), is(expected));
     }
-
 }
