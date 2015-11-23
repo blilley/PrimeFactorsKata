6d600a06fef4102ba0b267eb1f56a9ebd72499d1
Brett Lilley <brettlilley@bretts-mbp-2.cape-link.com>
2015-11-23 09:40:39 -0500

    Refactored

--- ts/src/main/app/primefactors.ts
+++ ts/src/main/app/primefactors.ts
@@ -1,13 +1,10 @@
 export default class PrimeFactors{
     public static generate(value:number): number[] {
         var primes: number[] = [];
-        var candidate : number = 2;
-        while(value > 1) {
-            while(value % candidate === 0){
+        for(var candidate:number = 2; value > 1; candidate++) {
+            for(;value % candidate === 0; value /= candidate) {
                 primes.push(candidate);
-                value /= candidate;
             }
-            candidate++;
         }
         return primes;
     }
