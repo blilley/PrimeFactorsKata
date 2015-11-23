c67c1c1087c2d380c1fc80153edfd4217f445bf7
Brett Lilley <brettlilley@bretts-mbp-2.cape-link.com>
2015-11-23 08:38:46 -0500

    First failing test

---
+++ ts/src/test/app/primefactors-spec.ts
@@ -0,0 +1,9 @@
+///<reference path='jasmine.d.ts'/>
+import PrimeFactors from '../../main/app/primefactors';
+describe('Prime Factors', () => {
+    describe('primeFactors of 1', () => {
+        it('should return empty array', () => {
+            expect(PrimeFactors.generate(1)).toBe([]);
+        });
+    });
+});
